85f30710997224b9c451768f688d768bc654c316
mls93 <mlsmipt@gmail.com>
2016-01-09 14:15:57 +0300

    Reworked data generating script to produce more convenient to xls data

--- generate_result.py
+++ generate_result.py
@@ -4,29 +4,25 @@ import subprocess
 import math
 import sys
 
-
-
 threads = [1,2,3,4,5,7,10,12,15,20]
 arr = ['LinkedBlockingQueue','ArrayBlockingQueue','PriorityBlockingQueue','PriorityQueue','LinkedList']
-operations=['pop']
-
-#p = subprocess.Popen(['java', '-jar', 'MultiQueue.jar',"1","1"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-#out, err = p.communicate()
-#print(str(out))
-
+time_arr=[]
+operations=['add','pop']
+n_tests = 50
 
 for o in operations:
-	for q in range(len(arr)):
-		fl = open(arr[q]+'_'+o+".txt",'w')
-		for t in threads:
+	fl = open("result_"+o+".txt",'w')
+	fl.write("N_Threads "+" ".join(arr)+'\n')
+	for t in threads:
+		time_arr = [str(t)]
+		for q in range(len(arr)):
 			res=0
-			for n in range(50):
-				
+			for n in range(n_tests):
 				p = subprocess.Popen(['java', '-jar', 'MultiQueue.jar',str(q+1),str(t),o], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 				out, err = p.communicate()
 				print(arr[q],t,n,o,out)
 				res+=int(out)
-			res/50
-			fl.write(str(res)+" "+str(t)+'\n')	
-		fl.close()
+			time_arr.append(str(res/n_tests))
+		fl.write(" ".join(time_arr)+'\n')		
+	fl.close()	
 
